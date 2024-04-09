73963024d8fe5fa675bdbde488b7aa8392d14f3b
Derek Nguyen <quanmn.libra@gmail.com>
2024-04-09 20:52:03 +0700

    Add sentry

--- api/index.js
+++ api/index.js
@@ -3,11 +3,35 @@ const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const multer = require('multer');
+const Sentry = require("@sentry/node");
+import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 const qrCodeController = require('./qrCodeController');
 
 dotenv.config()
 const app = express();
+
+Sentry.init({
+  dsn: "https://3ad0704ccbb6fefae1b52b086dcd5e2e@o472168.ingest.us.sentry.io/4507056899424256",
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Sentry.Integrations.Express({ app }),
+    nodeProfilingIntegration(),
+  ],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set sampling rate for profiling - this is relative to tracesSampleRate
+  profilesSampleRate: 1.0,
+});
+
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(bodyParser.raw());
@@ -21,6 +45,21 @@ app.get('/api/healthCheck', (req, res) => {
 app.post('/api/uploadImage', qrCodeController.uploadImage)
 app.post('/api/createQrCode', qrCodeController.createQrCode)
 app.post('/api/createQrCodeV2', qrCodeController.createQrCodeV2)
+
+app.get("/debug-sentry", function mainHandler(req, res) {
+  throw new Error("My first Sentry error!");
+});
+
+// The error handler must be registered before any other error middleware and after all controllers
+app.use(Sentry.Handlers.errorHandler());
+
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+  // The error id is attached to `res.sentry` to be returned
+  // and optionally displayed to the user for support.
+  res.statusCode = 500;
+  res.end(res.sentry + "\n");
+});
 
 // Start the server
 const appEnv = process.env.APP_ENV || 'development'
